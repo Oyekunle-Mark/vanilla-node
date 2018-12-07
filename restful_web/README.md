078ab01ec39ac09a4a9bbd2ad6bfc0139c3919ae
Oyekunle-Mark <oyekunlemac@gmail.com>
2018-12-08 00:36:17 +0100

    A restful app

---
+++ restful_web/README.md
@@ -0,0 +1,2 @@
+This is a simple RESTful web service which performs the CRUD operations with the post, get, put and delete methods respectively with a basic javascript array as storage.
+The cURL command line utility is used in place of a web browser to make the requests.
---
+++ restful_web/index.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var url = require('url');
+
+// declare a js array to hold data
+var items = [];
+
+var server = http.createServer(function(req, res) {
+	switch(req.method) {
+		case 'POST': 	// for create processes
+			var item = '';
+			req.setEncoding('utf8'); // encode the incoming data events as utf-8 strings
+			req.on('data', function(chunk) {
+				item += chunk;
+			});
+			req.on('end', function() {
+				items.push(item); //push to storage
+				res.end('OK\n');
+			});
+			break;
+		case 'GET': 	//for read operations
+			var body = items.map(function(item, i) {
+				return i + ')' + item;
+			}).join('\n');
+			res.setHeader('Content-Length', Buffer.byteLength(body));
+			res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+			res.end(body);
+			break;
+		case 'DELETE': 	//for delete operations
+			var path = url.parse(req.url).pathname;
+			var i = parseInt(path.slice(1), 10); // carry out type conversion on the pathname
+			if (isNaN(i)) {
+				res.statusCode = 400;
+				res.end('Invalid item id');
+			} else if (!items[i]) {
+				res.statusCode = 404;
+				res.end('Item not found');
+			} else {
+				items.splice(i, 1);
+				res.end('OK\n');
+			}
+			break;
+		case 'PUT': 	//for update operations
+			var path = url.parse(req.url).pathname;
+			var i = parseInt(path.slice(1), 10);
+			if (isNan(i)) { 	//check if number is valid
+				res.statusCode = 400;
+				res.end('Invalid item id');
+			} else if (!item[i]) { 	// if the index does not exist
+				res.statusCode = 404;
+				res.end('Item not found');
+			} else {
+				var item = '';
+				req.on('data', function(chunk) { 	// append the data to the stream
+					item += chunk;
+				req.setEncoding('utf8'); 	//encode the incoming data as utf-8 strings
+				});
+				req.on('end', function() { 	// when the data has been read
+					item.splice(i, i, item); // replace the index with the data stream
+					res.end('OK\n');
+				});
+			}
+				break;
+	}
+});
+
+server.listen(3000, function() {
+	console.log("Server listening on port 3000");
+});
